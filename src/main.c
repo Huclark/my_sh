ff23cf9d2edadf16cd691c0b4d011a6f9397ee83
Huclark <vhuclark@gmail.com>
2023-11-07 21:06:52 +0000

    main.c uploaded

---
+++ src/main.c
@@ -0,0 +1,13 @@
+#include "sh.h"
+
+/**
+ * main - main function for shell program
+ * @argc: Argument count
+ * @argv: Argument vector
+ * Return: The exit code (loop_status)
+ */
+
+int main(int argc, char **argv)
+{
+	return (sh_loop(argv, argc));
+}
